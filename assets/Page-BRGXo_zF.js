80e4f165baebe51596575d1453a6463c9b2c80e6
maifeeulasad <maifeeulasad@users.noreply.github.com>
2025-06-28 12:56:28 +0000

    deploy: 54d038962191d027b10673007f749f99fa0a8db7

---
+++ assets/Page-BRGXo_zF.js
@@ -0,0 +1,30 @@
+import{j as e}from"./index-BouKkZ2h.js";import{R as t}from"./RedditPostRenderer-Ro37FwL4.js";import"./index-BbV0Sjqj.js";const l=[{kind:"Listing",data:{after:null,dist:1,modhash:"",geo_filter:"",children:[{kind:"t3",data:{approved_at_utc:null,subreddit:"LocalLLaMA",selftext:`Hey everyone,
+
+If you're working with LLMs and want a clean, chat-style interface inside Jupyter notebooks, I’ve been experimenting with ChatUI integration — and it actually works really well for prototyping and testing.
+
+You get:
+
+A lightweight frontend (ChatUI)
+
+Inside Jupyter (no extra servers needed)
+
+Supports streaming responses from LLMs
+
+Great for testing prompts, workflows, or local models
+
+Has anyone else tried integrating UI layers like this into notebooks? Would love to know if you're using something lighter or more custom.`,user_reports:[],saved:!1,mod_reason_title:null,gilded:0,clicked:!1,title:"🛠️ ChatUI + Jupyter: A smooth way to test LLMs in your notebook interface",link_flair_richtext:[{e:"text",t:"Tutorial | Guide"}],subreddit_name_prefixed:"r/LocalLLaMA",hidden:!1,pwls:6,link_flair_css_class:"",downs:0,thumbnail_height:null,top_awarded_type:null,hide_score:!1,name:"t3_1llwtcd",quarantine:!1,link_flair_text_color:"light",upvote_ratio:1,author_flair_background_color:null,subreddit_type:"public",ups:8,total_awards_received:0,media_embed:{},thumbnail_width:null,author_flair_template_id:null,is_original_content:!1,author_fullname:"t2_12m02dhx",secure_media:null,is_reddit_media_domain:!1,is_meta:!1,category:null,secure_media_embed:{},link_flair_text:"Tutorial | Guide",can_mod_post:!1,score:8,approved_by:null,is_created_from_ads_ui:!1,author_premium:!1,thumbnail:"self",edited:!1,author_flair_css_class:null,author_flair_richtext:[],gildings:{},content_categories:null,is_self:!0,mod_note:null,created:1751038207,link_flair_type:"richtext",wls:6,removed_by_category:null,banned_by:null,author_flair_type:"text",domain:"self.LocalLLaMA",allow_live_comments:!1,selftext_html:`&lt;!-- SC_OFF --&gt;&lt;div class="md"&gt;&lt;p&gt;Hey everyone,&lt;/p&gt;
+
+&lt;p&gt;If you&amp;#39;re working with LLMs and want a clean, chat-style interface inside Jupyter notebooks, I’ve been experimenting with ChatUI integration — and it actually works really well for prototyping and testing.&lt;/p&gt;
+
+&lt;p&gt;You get:&lt;/p&gt;
+
+&lt;p&gt;A lightweight frontend (ChatUI)&lt;/p&gt;
+
+&lt;p&gt;Inside Jupyter (no extra servers needed)&lt;/p&gt;
+
+&lt;p&gt;Supports streaming responses from LLMs&lt;/p&gt;
+
+&lt;p&gt;Great for testing prompts, workflows, or local models&lt;/p&gt;
+
+&lt;p&gt;Has anyone else tried integrating UI layers like this into notebooks? Would love to know if you&amp;#39;re using something lighter or more custom.&lt;/p&gt;
+&lt;/div&gt;&lt;!-- SC_ON --&gt;`,likes:null,suggested_sort:null,banned_at_utc:null,view_count:null,archived:!1,no_follow:!1,is_crosspostable:!1,pinned:!1,over_18:!1,all_awardings:[],awarders:[],media_only:!1,link_flair_template_id:"449b05a6-bf8e-11ed-b4bd-66961e47bd50",can_gild:!1,spoiler:!1,locked:!1,author_flair_text:null,treatment_tags:[],visited:!1,removed_by:null,num_reports:null,distinguished:null,subreddit_id:"t5_81eyvm",author_is_blocked:!1,mod_reason_by:null,removal_reason:null,link_flair_background_color:"#0079d3",id:"1llwtcd",is_robot_indexable:!0,num_duplicates:0,report_reasons:null,author:"techlatest_net",discussion_type:null,num_comments:0,send_replies:!1,media:null,contest_mode:!1,author_patreon_flair:!1,author_flair_text_color:null,permalink:"/r/LocalLLaMA/comments/1llwtcd/chatui_jupyter_a_smooth_way_to_test_llms_in_your/",stickied:!1,url:"https://www.reddit.com/r/LocalLLaMA/comments/1llwtcd/chatui_jupyter_a_smooth_way_to_test_llms_in_your/",subreddit_subscribers:492232,created_utc:1751038207,num_crossposts:0,mod_reports:[],is_video:!1}}],before:null}},{kind:"Listing",data:{after:null,dist:null,modhash:"",geo_filter:"",children:[],before:null}}],s=()=>e.jsx(t,{data:l});export{s as default};
