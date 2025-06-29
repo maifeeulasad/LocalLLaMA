c1dbab6e3b8f19ca5c4fc858fdd101c03a9240b0
maifeeulasad <maifeeulasad@users.noreply.github.com>
2025-06-29 12:58:49 +0000

    deploy: 9b58ab4e8613480b30d2ebac9ce9b1bf4d29c6f0

---
+++ assets/Page-hz46ODZo.js
@@ -0,0 +1,42 @@
+import{j as e}from"./index-C7fe81h5.js";import{R as t}from"./RedditPostRenderer-Dwf2rxtd.js";import"./index-DtfsRTYt.js";const l=[{kind:"Listing",data:{after:null,dist:1,modhash:"",geo_filter:"",children:[{kind:"t3",data:{approved_at_utc:null,subreddit:"LocalLLaMA",selftext:`Hey everyone 👋
+
+I’m currently building multi-agent systems using LangGraph, mostly for personal/work projects. Lately I’ve been thinking a lot about how many developers actually rely on AI tools (like ChatGPT, Gmini, Claude, etc) as coding copilots or even as design companions.
+
+I sometimes feel torn between:
+
+* *“Am I genuinely building this on my own skills?”* vs
+* *“Am I just an overglorified prompt-writer leaning on LLMs to solve the hard parts?”*
+
+I suspect it’s partly impostor syndrome.  
+But honestly, I’d love to hear how others approach it:
+
+* Do you integrate ChatGPT / Gmini / others into your actual **development cycle** when creating LangGraph agents? (or any agent framework really)
+* What has your experience been like — more productivity, more confusion, more debugging hell?
+* Do you ever worry it dilutes your own engineering skill, or do you see it as just another power tool?
+
+Also curious if you use it beyond code generation — e.g. for reasoning about graph state transitions, crafting system prompts, evaluating multi-agent dialogue flows, etc.
+
+Would appreciate any honest thoughts or battle stories. Thanks!`,user_reports:[],saved:!1,mod_reason_title:null,gilded:0,clicked:!1,title:"Do you use AI (like ChatGPT, Gmini, etc) to develop your LangGraph agents? Or is it just my impostor syndrome talking?",link_flair_richtext:[{e:"text",t:"Question | Help"}],subreddit_name_prefixed:"r/LocalLLaMA",hidden:!1,pwls:6,link_flair_css_class:"",downs:0,thumbnail_height:null,top_awarded_type:null,hide_score:!1,name:"t3_1ln3pur",quarantine:!1,link_flair_text_color:"dark",upvote_ratio:.6,author_flair_background_color:null,subreddit_type:"public",ups:2,total_awards_received:0,media_embed:{},thumbnail_width:null,author_flair_template_id:null,is_original_content:!1,author_fullname:"t2_1qyykcj4",secure_media:null,is_reddit_media_domain:!1,is_meta:!1,category:null,secure_media_embed:{},link_flair_text:"Question | Help",can_mod_post:!1,score:2,approved_by:null,is_created_from_ads_ui:!1,author_premium:!1,thumbnail:"self",edited:!1,author_flair_css_class:null,author_flair_richtext:[],gildings:{},content_categories:null,is_self:!0,mod_note:null,created:1751163573,link_flair_type:"richtext",wls:6,removed_by_category:null,banned_by:null,author_flair_type:"text",domain:"self.LocalLLaMA",allow_live_comments:!1,selftext_html:`&lt;!-- SC_OFF --&gt;&lt;div class="md"&gt;&lt;p&gt;Hey everyone 👋&lt;/p&gt;
+
+&lt;p&gt;I’m currently building multi-agent systems using LangGraph, mostly for personal/work projects. Lately I’ve been thinking a lot about how many developers actually rely on AI tools (like ChatGPT, Gmini, Claude, etc) as coding copilots or even as design companions.&lt;/p&gt;
+
+&lt;p&gt;I sometimes feel torn between:&lt;/p&gt;
+
+&lt;ul&gt;
+&lt;li&gt;&lt;em&gt;“Am I genuinely building this on my own skills?”&lt;/em&gt; vs&lt;/li&gt;
+&lt;li&gt;&lt;em&gt;“Am I just an overglorified prompt-writer leaning on LLMs to solve the hard parts?”&lt;/em&gt;&lt;/li&gt;
+&lt;/ul&gt;
+
+&lt;p&gt;I suspect it’s partly impostor syndrome.&lt;br/&gt;
+But honestly, I’d love to hear how others approach it:&lt;/p&gt;
+
+&lt;ul&gt;
+&lt;li&gt;Do you integrate ChatGPT / Gmini / others into your actual &lt;strong&gt;development cycle&lt;/strong&gt; when creating LangGraph agents? (or any agent framework really)&lt;/li&gt;
+&lt;li&gt;What has your experience been like — more productivity, more confusion, more debugging hell?&lt;/li&gt;
+&lt;li&gt;Do you ever worry it dilutes your own engineering skill, or do you see it as just another power tool?&lt;/li&gt;
+&lt;/ul&gt;
+
+&lt;p&gt;Also curious if you use it beyond code generation — e.g. for reasoning about graph state transitions, crafting system prompts, evaluating multi-agent dialogue flows, etc.&lt;/p&gt;
+
+&lt;p&gt;Would appreciate any honest thoughts or battle stories. Thanks!&lt;/p&gt;
+&lt;/div&gt;&lt;!-- SC_ON --&gt;`,likes:null,suggested_sort:null,banned_at_utc:null,view_count:null,archived:!1,no_follow:!0,is_crosspostable:!1,pinned:!1,over_18:!1,all_awardings:[],awarders:[],media_only:!1,link_flair_template_id:"2c9831e6-bf92-11ed-98e6-d2b8bcc02ae1",can_gild:!1,spoiler:!1,locked:!1,author_flair_text:null,treatment_tags:[],visited:!1,removed_by:null,num_reports:null,distinguished:null,subreddit_id:"t5_81eyvm",author_is_blocked:!1,mod_reason_by:null,removal_reason:null,link_flair_background_color:"#5a74cc",id:"1ln3pur",is_robot_indexable:!0,num_duplicates:0,report_reasons:null,author:"Ranteck",discussion_type:null,num_comments:0,send_replies:!0,media:null,contest_mode:!1,author_patreon_flair:!1,author_flair_text_color:null,permalink:"/r/LocalLLaMA/comments/1ln3pur/do_you_use_ai_like_chatgpt_gmini_etc_to_develop/",stickied:!1,url:"https://www.reddit.com/r/LocalLLaMA/comments/1ln3pur/do_you_use_ai_like_chatgpt_gmini_etc_to_develop/",subreddit_subscribers:492572,created_utc:1751163573,num_crossposts:0,mod_reports:[],is_video:!1}}],before:null}},{kind:"Listing",data:{after:null,dist:null,modhash:"",geo_filter:"",children:[],before:null}}],i=()=>e.jsx(t,{data:l});export{i as default};
