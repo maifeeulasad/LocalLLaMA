c42c4f0036420cdd3f6e065d39e4349675c196c5
maifeeulasad <maifeeulasad@users.noreply.github.com>
2025-06-30 12:37:48 +0000

    deploy: 207f0ce3aa4d1276424fd97d8833a9cce1ef6e63

---
+++ assets/Page-Dl4tNRiA.js
@@ -0,0 +1,44 @@
+import{j as e}from"./index-BrqAgJWx.js";import{R as l}from"./RedditPostRenderer-chN7TDMj.js";import"./index-DlMtF8rT.js";const t=[{kind:"Listing",data:{after:null,dist:1,modhash:"",geo_filter:"",children:[{kind:"t3",data:{approved_at_utc:null,subreddit:"LocalLLaMA",selftext:`Hello, I am looking for &lt;= 4B coding models. I realize that none of these will be practical for now just looking for some to do experiments.
+
+Here is what i found so far:
+\r- Menlo / Jan-nano — 4.02 B (Not really coding but I expect it to be better than others)\r
+- Gemma — 4 B / 2 B\r
+- Qwen 3 — 4 B / 0.6 B\r- Phi-4 Mini — 3.8 B\r- Phi-3.5 Mini — 3.5 B\r- Llama-3.2 — 3.2 B\r- Starcoder — 3 B / 1 B\r- Starcoder 2 — 3 B\r- Stable-Code — 3 B\r- Granite — 3 B / 2.53 B\r- Cogito — 3 B\r- DeepSeek Coder — 2.6 B / 1.3 B\r- DeepSeek R1 Distill (Qwen-tuned) — 1.78 B\r- Qwen 2.5 — 1.5 B / 0.5 B\r- Yi-Coder — 1.5 B\r- Deepscaler — 1.5 B\r- Deepcoder — 1.5 B\r- CodeGen2 — 1 B\r- BitNet-B1.58 — 0.85 B\r- ERNIE-4.5 — 0.36 B
+
+Has anyone tried any of these or compared &lt;= 4B models on coding tasks? 
+
+\r
+`,user_reports:[],saved:!1,mod_reason_title:null,gilded:0,clicked:!1,title:"What is the current best local coding model with &lt;= 4B parameters?",link_flair_richtext:[{e:"text",t:"Question | Help"}],subreddit_name_prefixed:"r/LocalLLaMA",hidden:!1,pwls:6,link_flair_css_class:"",downs:0,thumbnail_height:null,top_awarded_type:null,hide_score:!0,name:"t3_1lo5vnf",quarantine:!1,link_flair_text_color:"dark",upvote_ratio:1,author_flair_background_color:null,subreddit_type:"public",ups:3,total_awards_received:0,media_embed:{},thumbnail_width:null,author_flair_template_id:null,is_original_content:!1,author_fullname:"t2_1g2h9wsp6m",secure_media:null,is_reddit_media_domain:!1,is_meta:!1,category:null,secure_media_embed:{},link_flair_text:"Question | Help",can_mod_post:!1,score:3,approved_by:null,is_created_from_ads_ui:!1,author_premium:!1,thumbnail:"self",edited:!1,author_flair_css_class:null,author_flair_richtext:[],gildings:{},content_categories:null,is_self:!0,mod_note:null,created:1751285432,link_flair_type:"richtext",wls:6,removed_by_category:null,banned_by:null,author_flair_type:"text",domain:"self.LocalLLaMA",allow_live_comments:!1,selftext_html:`&lt;!-- SC_OFF --&gt;&lt;div class="md"&gt;&lt;p&gt;Hello, I am looking for &amp;lt;= 4B coding models. I realize that none of these will be practical for now just looking for some to do experiments.&lt;/p&gt;
+
+&lt;p&gt;Here is what i found so far:&lt;/p&gt;
+
+&lt;ul&gt;
+&lt;li&gt;Menlo / Jan-nano — 4.02 B (Not really coding but I expect it to be better than others)&lt;/li&gt;
+&lt;li&gt;Gemma — 4 B / 2 B&lt;/li&gt;
+&lt;li&gt;Qwen 3 — 4 B / 0.6 B&lt;/li&gt;
+&lt;li&gt;Phi-4 Mini — 3.8 B&lt;/li&gt;
+&lt;li&gt;Phi-3.5 Mini — 3.5 B&lt;/li&gt;
+&lt;li&gt;Llama-3.2 — 3.2 B&lt;/li&gt;
+&lt;li&gt;Starcoder — 3 B / 1 B&lt;/li&gt;
+&lt;li&gt;Starcoder 2 — 3 B&lt;/li&gt;
+&lt;li&gt;Stable-Code — 3 B&lt;/li&gt;
+&lt;li&gt;Granite — 3 B / 2.53 B&lt;/li&gt;
+&lt;li&gt;Cogito — 3 B&lt;/li&gt;
+&lt;li&gt;DeepSeek Coder — 2.6 B / 1.3 B&lt;/li&gt;
+&lt;li&gt;DeepSeek R1 Distill (Qwen-tuned) — 1.78 B&lt;/li&gt;
+&lt;li&gt;Qwen 2.5 — 1.5 B / 0.5 B&lt;/li&gt;
+&lt;li&gt;Yi-Coder — 1.5 B&lt;/li&gt;
+&lt;li&gt;Deepscaler — 1.5 B&lt;/li&gt;
+&lt;li&gt;Deepcoder — 1.5 B&lt;/li&gt;
+&lt;li&gt;CodeGen2 — 1 B&lt;/li&gt;
+&lt;li&gt;BitNet-B1.58 — 0.85 B&lt;/li&gt;
+&lt;li&gt;ERNIE-4.5 — 0.36 B&lt;/li&gt;
+&lt;/ul&gt;
+
+&lt;p&gt;Has anyone tried any of these or compared &amp;lt;= 4B models on coding tasks? &lt;/p&gt;
+&lt;/div&gt;&lt;!-- SC_ON --&gt;`,likes:null,suggested_sort:null,banned_at_utc:null,view_count:null,archived:!1,no_follow:!1,is_crosspostable:!1,pinned:!1,over_18:!1,all_awardings:[],awarders:[],media_only:!1,link_flair_template_id:"2c9831e6-bf92-11ed-98e6-d2b8bcc02ae1",can_gild:!1,spoiler:!1,locked:!1,author_flair_text:null,treatment_tags:[],visited:!1,removed_by:null,num_reports:null,distinguished:null,subreddit_id:"t5_81eyvm",author_is_blocked:!1,mod_reason_by:null,removal_reason:null,link_flair_background_color:"#5a74cc",id:"1lo5vnf",is_robot_indexable:!0,num_duplicates:0,report_reasons:null,author:"Wooden-Key751",discussion_type:null,num_comments:1,send_replies:!0,media:null,contest_mode:!1,author_patreon_flair:!1,author_flair_text_color:null,permalink:"/r/LocalLLaMA/comments/1lo5vnf/what_is_the_current_best_local_coding_model_with/",stickied:!1,url:"https://www.reddit.com/r/LocalLLaMA/comments/1lo5vnf/what_is_the_current_best_local_coding_model_with/",subreddit_subscribers:492928,created_utc:1751285432,num_crossposts:0,mod_reports:[],is_video:!1}}],before:null}},{kind:"Listing",data:{after:null,dist:null,modhash:"",geo_filter:"",children:[{kind:"t1",data:{subreddit_id:"t5_81eyvm",approved_at_utc:null,author_is_blocked:!1,comment_type:null,awarders:[],mod_reason_by:null,banned_by:null,author_flair_type:"text",total_awards_received:0,subreddit:"LocalLLaMA",author_flair_template_id:null,likes:null,replies:"",user_reports:[],saved:!1,id:"n0kcsjj",banned_at_utc:null,mod_reason_title:null,gilded:0,archived:!1,collapsed_reason_code:null,no_follow:!0,author:"loyalekoinu88",can_mod_post:!1,created_utc:1751285868,send_replies:!0,parent_id:"t3_1lo5vnf",score:1,author_fullname:"t2_1x5p0ubz",approved_by:null,mod_note:null,all_awardings:[],collapsed:!1,body:`Jan-Nano is just a specialty QWEN3 4B model.
+
+My best guess would be to use ones specifically trained on coding since that isn’t a lot of parameters for general models. I’d also imagine coding models that have good tool use would be best since you can pull in more coding context.`,edited:!1,top_awarded_type:null,author_flair_css_class:null,name:"t1_n0kcsjj",is_submitter:!1,downs:0,author_flair_richtext:[],author_patreon_flair:!1,body_html:`&lt;div class="md"&gt;&lt;p&gt;Jan-Nano is just a specialty QWEN3 4B model.&lt;/p&gt;
+
+&lt;p&gt;My best guess would be to use ones specifically trained on coding since that isn’t a lot of parameters for general models. I’d also imagine coding models that have good tool use would be best since you can pull in more coding context.&lt;/p&gt;
+&lt;/div&gt;`,removal_reason:null,collapsed_reason:null,distinguished:null,associated_award:null,stickied:!1,author_premium:!1,can_gild:!1,gildings:{},unrepliable_reason:null,author_flair_text_color:null,score_hidden:!1,permalink:"/r/LocalLLaMA/comments/1lo5vnf/what_is_the_current_best_local_coding_model_with/n0kcsjj/",subreddit_type:"public",locked:!1,report_reasons:null,created:1751285868,author_flair_text:null,treatment_tags:[],link_id:"t3_1lo5vnf",subreddit_name_prefixed:"r/LocalLLaMA",controversiality:0,depth:0,author_flair_background_color:null,collapsed_because_crowd_control:null,mod_reports:[],num_reports:null,ups:1}}],before:null}}],i=()=>e.jsx(l,{data:t});export{i as default};
