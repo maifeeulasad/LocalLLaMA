8a42638f7f702635275e4640dc818cfcd7accbcb
Maifee Ul Asad <maifeeulasad@gmail.com>
2025-07-31 17:29:10 +0600

    [vite]: not generating source map for build;

--- vite.config.ts
+++ vite.config.ts
@@ -31,6 +31,7 @@ export default defineConfig({
   // comment this out if that isn't relevant for your project
   build: {
     outDir: './build',
+    sourcemap: false,
   },
   base: './',
   plugins: [
